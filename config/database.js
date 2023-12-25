07bbe1202212ef40d90571a1c9c3fb3f27148fd3
suyash <suyash@openspaceservices.com>
2023-12-25 20:58:40 +0530

    initial commit

---
+++ config/database.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: '127.0.0.1',
+    port: 3306,
+    dialect: 'mysql',
+    dialectOptions: {
+      bigNumberStrings: true
+    },
+    migrationStorageTableName: 'migrations'
+  },
+  test: {
+    username: process.env.CI_DB_USERNAME,
+    password: process.env.CI_DB_PASSWORD,
+    database: process.env.CI_DB_NAME,
+    host: '127.0.0.1',
+    port: 3306,
+    dialect: 'mysql',
+    dialectOptions: {
+      bigNumberStrings: true
+    }
+  }
+};
